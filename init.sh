f14556cc9fc9bd8b7009eca02b56e9bcee7917c2
verenikindmitriy <verenikindmitriy@mail.ru>
2016-03-10 09:22:00 +0300

    4.1

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.py  /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
 cd /home/box/web/
 sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:application
