072042c2a6948cd08456b0479da6167d4c44d7a1
verenikindmitriy <verenikindmitriy@mail.ru>
2016-03-10 09:31:06 +0300

    4.2

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 cd /home/box/web/
-sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:application
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:application
